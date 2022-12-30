71c3d5fe75c53d2af4080774565adc451566a9fc
jeroen <jeroen.smissaert@gmail.com>
2022-12-30 17:36:08 +0100

    Create User, add Validators

--- Fullstack_Opdracht1/ViewModels/User.cs
+++ Fullstack_Opdracht1/ViewModels/User.cs
@@ -1,14 +1,33 @@
-﻿namespace Fullstack_Opdracht1.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fullstack_Opdracht1.ViewModels
 {
     public class User
     {
+        [Required, MinLength(3), MaxLength(50)]
         public string name { get; set; }
+
+        [Required, MinLength(3), MaxLength(50)]
         public string firstName { get; set; }
+
+        [Required, Range(18, 200)]
         public int age { get; set; }
+
+        [Required, EmailAddress]
         public string email { get; set; }
+
+        [Required]
+        [Remote("ValidateCountry", "Validator")]
         public string country { get; set; }
+
+        [Required, Range(1000, 9999)]
         public int zipCode { get; set; }
 
+        [Required]
+        [RegularExpression(@"^([\+]?)([0-9]{5,7})([\/])([0-9]{6})$")]
         public string mobilePhoneNumber { get; set; }
 
     }
