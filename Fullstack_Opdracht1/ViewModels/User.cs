dab17841812ba740e5d216388e59285d3cd1e9c8
jeroen <jeroen.smissaert@gmail.com>
2022-12-30 16:51:48 +0100

    Add project files.

---
+++ Fullstack_Opdracht1/ViewModels/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Fullstack_Opdracht1.ViewModels
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string firstName { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+        public string country { get; set; }
+        public int zipCode { get; set; }
+
+        public string mobilePhoneNumber { get; set; }
+
+    }
+}
