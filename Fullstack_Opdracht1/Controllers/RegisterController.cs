a3aab400075976116c68ee21e2fe5bd7e8b2f3ed
jeroen <jeroen.smissaert@gmail.com>
2022-12-30 17:51:52 +0100

    Add registercontroller

---
+++ Fullstack_Opdracht1/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fullstack_Opdracht1.ViewModels;
+
+namespace Fullstack_Opdracht1.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserVM user)
+        {
+            if(!ModelState.IsValid)
+            {
+                ViewData["registrationMessage"] = "Form is invald";
+                return View();
+            }
+
+
+            return View("Index", user);
+        }
+    }
+}
